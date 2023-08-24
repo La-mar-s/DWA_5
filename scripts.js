49249777b8ccfcf64802f2412e4976388e0c0e74
La-mar-s <sony.lamar.1995@gmail.com>
2023-08-24 14:39:22 +0200

    fixed errors in try and catch

--- scripts.js
+++ scripts.js
@@ -7,20 +7,37 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+  console.clear;
 
   try{
 
-    if (dividend = "" || divider == "")
-        throw "Division not performed. Both values are required in inputs. Try again";
-    if (divider < 0)
-        throw "Division not performed. Invalid number provided. Try again";
-    if (isNaN(dividend || divider))
-        throw "Something critical went wrong. Please reload the page";
+    if (dividend === "" || divider === "") {
+       result.innerText = "Division not performed. Both values are required in inputs. Try again";   
+    }
+
+    if (divider <= 0) {
+        result.innerText = "Division not performed. Invalid number provided. Try again";
+        throw new Error("Invalid input provided"); 
+    }
+
+    if (isNaN(dividend) || isNaN(divider)) {
+        document.body.innerHTML = "Something critical went wrong. Please reload the page."; 
+        throw new Error ('Please reload the page') 
+    }
+
+    const sum = dividend / divider
+
+        if (!Number.isNaN(sum) && Number.isInteger(sum)){
+        result.innerText = sum
+        } else {
+            result.innerText ="Division not performed. Invalid number provided. Try again";
+            throw new Error ("Division not performed. Invalid number provided. Try again")
+        }
 
   } catch (error){
         console.error(error);
   }
+
 });
 
 
